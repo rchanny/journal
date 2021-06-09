19839b18a2adc38dcc0aa341fe97c9d1fa581c99
Isha WBS Test Env <alerts@sambaash.com>
2021-06-09 07:57:52 +0000

    added deployed.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd /edx/app/edxapp/edx-platform/lms/
+cp -rf custom_app/journal/djangoapps/journal djangoapps/
+cp -rf custom_app/journal/templates/journal templates/
+cp -rf custom_app/journal/templates/dashboard/_dashboard_journal_listing.html templates/dashboard/
+cp -rf custom_app/journal/templates/journal.html templates/
+
